31de19289eb84caad786997189eea2f064e640d9
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-01-26 00:08:13 +0900

    first commit.

---
+++ src/js/lp.js
@@ -0,0 +1,17 @@
+$(function () {
+    var $main = $('.photo.main, .video.main');
+    if ($main.length) {
+        $main.remove();
+        $('h1').before($main);
+    }
+
+    var $ios = $('.link.ios');
+    if ($ios.length) {
+        $ios.find('i').attr('class', 'fa fa-apple');
+    }
+
+    var $android = $('.link.android');
+    if ($android.length) {
+        $android.find('i').attr('class', 'fa fa-android');
+    }
+});
