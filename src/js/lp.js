c58634bb2bc2d1b0e0c605dacd2cb6f194215d47
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-02-19 01:18:19 +0900

    ページのガイド追加。

--- src/js/lp.js
+++ src/js/lp.js
@@ -50,17 +50,23 @@ $(function () {
     // init cover
     (function () {
         var $headerSlide = $('.header__slide');
+        var $headerGuide = $('.header__guide');
+
         var $slidePosts = $('.post--photo.cover');
         $slidePosts.each(function () {
             var $slidePost = $(this);
             $slidePost.hide();
 
-            var $li = $('<li />');
-            $li.addClass('slide');
-            $li.css({
+            var $slide = $('<li />');
+            $slide.addClass('slide');
+            $slide.css({
                 backgroundImage: 'url(' + $slidePost.find('img').attr('src') + ')'
             });
-            $headerSlide.prepend($li);
+            $headerSlide.prepend($slide);
+
+            var $guide = $('<li />');
+            $guide.addClass('guide');
+            $headerGuide.append($guide);
         });
 
         if ($slidePosts.length < 2) {
@@ -69,9 +75,15 @@ $(function () {
 
         var waitTime = 2000;
         var fadeTime = 2000;
+        var index = 0;
 
+        $('.guide:first').addClass('active');
         (function paging () {
             setTimeout(function () {
+                index = (index + 1) % $headerSlide.children().length;
+                $('.guide.active').removeClass('active');
+                $('.guide:nth-child(' + (index + 1) + ')').addClass('active');
+
                 var $first = $($headerSlide.children(':last'));
                 $first.fadeOut(fadeTime, function () {
                     $headerSlide.prepend($first);
